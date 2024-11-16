85f07f584615d3b9cb74c7b67e60562f3a63d313
Jayesh Bhole <jayeshbhole123@gmail.com>
2024-11-16 16:42:16 +0700

    Update page.tsx

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -70,7 +70,7 @@ export default function HomePage() {
         className="inline-flex h-10 items-center justify-center gap-2 whitespace-nowrap rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground ring-offset-background transition-colors hover:bg-primary/90 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
         onClick={() =>
           window.open(
-            `https://redirect-tapas-world.vercel.app?redirect=${encodeURIComponent(window.origin + `/app/world-user/2-verify-nfc`)}`,
+            `https://redirect-tapas-world.vercel.app/redirect?redirect=${encodeURIComponent(window.origin + `/app/world-user/2-verify-nfc`)}`,
             "_system",
           )
         }
