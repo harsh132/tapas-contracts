9e43ed559b99356c0a102e8328196d163359e230
Jayesh Bhole <jayeshbhole123@gmail.com>
2024-11-16 10:40:25 +0700

    fix: notifs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={poppins.className}>
+    <html lang="en" className={poppins.className} suppressHydrationWarning>
       <body className="">
         <ThemeProvider
           attribute="class"
